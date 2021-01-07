a5fe85b78fe73f4a888a761b21b9ffb31e27d987
Jacek Polak <jacek_polak@epam.com>
2021-01-07 22:48:16 +0100

    feat: Add fetchye simple connection to parrot

--- security-web-portal/src/components/SecurityWebPortal.jsx
+++ security-web-portal/src/components/SecurityWebPortal.jsx
@@ -1,13 +1,36 @@
 import React from 'react';
 import { RenderModule, composeModules } from 'holocron';
+import { useFetchye } from 'fetchye';
 import childRoutes from '../childRoutes';
 
-const SecurityWebPortal = () => (
-  <div>
-    <h1>Welcome to One App!</h1>
-    <RenderModule moduleName="navigation" />
-  </div>
-);
+const SHIPS_API = 'http://localhost:3002/ship_log';
+
+const SecurityWebPortal = () => {
+  const { isLoading, data, error } = useFetchye(SHIPS_API);
+  const ships = data && data.body;
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+
+  if (error) {
+    return <p>Error fetching Ships</p>;
+  }
+
+  return (
+    <div>
+      <h1>Welcome to One App!</h1>
+      {ships &&
+        ships.map((shipItem, index) => (
+          <div key={`ship-${index}`}>
+            <p>Name - {shipItem.name}</p>
+            <p>Captain - {shipItem.captain}</p>
+          </div>
+        ))}
+      <RenderModule moduleName="navigation" />
+    </div>
+  );
+};
 
 // Read about childRoutes:
 // https://github.com/americanexpress/one-app/blob/main/docs/api/modules/Routing.md#childroutes
