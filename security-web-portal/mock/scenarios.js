d023f5136344bd23f6d2916e82dd66ee2d7a4568
Jacek Polak <jacek_polak@epam.com>
2021-01-07 22:21:31 +0100

    feat: Add parrot middleware and scenarios

---
+++ security-web-portal/mock/scenarios.js
@@ -0,0 +1,47 @@
+// Read the following to find out more:
+// Configuring scenarios: https://github.com/americanexpress/parrot/blob/master/SCENARIOS.md
+
+module.exports = {
+  'has one ship': [
+    {
+      request: '/ship_log',
+      response: {
+        body: [{ name: 'The Jolly Roger', captain: 'Captain Hook' }],
+      },
+    },
+  ],
+  'has more ships': [
+    {
+      request: '/ship_log',
+      response: {
+        body: [
+          { name: 'The Jolly Roger', captain: 'Captain Hook' },
+          { name: 'The Black Pearl', captain: 'Jack Sparrow' },
+          { name: 'Flying Dutchman', captain: 'Davy Jones' },
+          { name: 'The Wanderer', captain: 'Captain Ron' },
+        ],
+      },
+    },
+  ],
+  'has more ships with any mime type': [
+    {
+      request: '/ship_log',
+      response: {
+        contentType: 'text/plain',
+        body: [
+          '# Ship Log',
+          "* name: 'The Jolly Roger', captain: 'Captain Hook'",
+          "* name: 'The Black Pearl', captain: 'Jack Sparrow'",
+        ].join('\n'),
+      },
+    },
+  ],
+  'has a server error': [
+    {
+      request: '/ship_log',
+      response: {
+        status: 500,
+      },
+    },
+  ],
+};
--- security-web-portal/src/childRoutes.jsx
+++ security-web-portal/src/childRoutes.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import { Route } from '@americanexpress/one-app-router';
 
-const childRoutes = () => [
-  <Route path="/" />,
-];
+const childRoutes = () => [<Route path="/" />];
 
 export default childRoutes;
